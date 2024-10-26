75dcc818dd123724b223e527bac21a13bc14fd5b
Huy Nguyen <huynguyen@MacBookPro.lan>
2024-10-26 12:44:24 -0400

    Debug 1

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,5 +1,5 @@
 // Libraries imports
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 // local imports
 import App from './App'
@@ -16,12 +16,14 @@ let data: Product[] = []
 if (typeof window !== 'undefined') {
   // The data variable below will contain the data that was requested server-side, and passed to the client-side as a prop, in the <App data={data} /> component.
   data = window.__data__ || []
-  console.info('after', data)
-}
+  console.info('window after', data)
 
-hydrateRoot(
-  document.getElementById('root') as HTMLElement,
-  <StrictMode>
-    <App data={data} />
-  </StrictMode>
-)
+  hydrateRoot(
+    document.getElementById('root') as HTMLElement,
+    <StrictMode>
+      <App data={data} />
+    </StrictMode>
+  )
+} else {
+  console.error('Window is undefined')
+}
